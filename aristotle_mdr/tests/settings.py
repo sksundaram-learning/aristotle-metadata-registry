a53fc829134324b0c5efdba7b0d00baeb0ebc11b
theodore.therone@gmail.com <aristotle@ip-10-186-41-242.ec2.internal>
2014-12-08 10:04:17 +0000

    more coverage for DEC wizard"

--- aristotle_mdr/tests/settings.py
+++ aristotle_mdr/tests/settings.py
@@ -30,3 +30,5 @@ HAYSTACK_CONNECTIONS = {
 PASSWORD_HASHERS = (
     'django.contrib.auth.hashers.MD5PasswordHasher',
 )
+
+ARISTOTLE_SETTINGS['SEPARATORS']['DataElementConcept'] = '--'
--- aristotle_mdr/tests/test_wizards.py
+++ aristotle_mdr/tests/test_wizards.py
@@ -112,8 +112,125 @@ class DataElementWizardPage(ConceptWizardPage,TestCase):
 
 class DataElementConceptWizardPage(ConceptWizardPage,TestCase):
     wizard_url_name="createDataElementConcept"
+    wizard_form_name="data_element_concept_wizard"
     def test_editor_can_make_object(self):
         pass
+    def test_editor_can_make_object__has_prior_components(self):
+        pass
+    def test_editor_can_make_object__no_prior_components(self):
+        self.login_editor()
+        step_1_data = {
+            self.wizard_form_name+'-current_step': 'oc_p_search',
+        }
+
+        response = self.client.post(self.wizard_url, step_1_data)
+        wizard = response.context['wizard']
+        self.assertEqual(wizard['steps'].current, 'oc_p_search')
+        self.assertTrue('oc_name' in response.context['wizard']['form'].errors.keys())
+        self.assertTrue('pr_name' in response.context['wizard']['form'].errors.keys())
+
+        # must submit a name
+        step_1_data.update({'oc_p_search-oc_name':"Animagus"})
+        step_1_data.update({'oc_p_search-pr_name':"Animal type"})
+        # success!
+
+        response = self.client.post(self.wizard_url, step_1_data)
+        wizard = response.context['wizard']
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(wizard['steps'].current, 'oc_p_results')
+        self.assertContains(response,"No matching object classes were found")
+        self.assertContains(response,"No matching properties were found")
+
+        step_2_data = {
+            self.wizard_form_name+'-current_step': 'oc_p_results',
+        } # nothing else needed, as we aren't picking a component.
+
+        response = self.client.post(self.wizard_url, step_2_data)
+        wizard = response.context['wizard']
+        self.assertEqual(wizard['steps'].current, 'make_oc')
+
+        # Now we make the object class
+        step_3_data = {
+            self.wizard_form_name+'-current_step': 'make_oc',
+            'make_oc-name':"Animagus",
+        }
+
+        response = self.client.post(self.wizard_url, step_3_data)
+        wizard = response.context['wizard']
+        self.assertTrue('description' in response.context['wizard']['form'].errors.keys())
+        self.assertTrue('workgroup' in response.context['wizard']['form'].errors.keys())
+
+        # no "test item" yet.
+        self.assertFalse(models._concept.objects.filter(name="Test Item").exists())
+
+        # must submit a description at this step. But we are using a non-permitted workgroup.
+        step_3_data.update({
+            'make_oc-description':"A wizard who can change shape.",
+            'make_oc-workgroup':self.wg2.pk
+            })
+        response = self.client.post(self.wizard_url, step_3_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('workgroup' in response.context['wizard']['form'].errors.keys())
+
+        # must submit a description at this step. With the right workgroup
+        step_3_data.update({
+            'make_oc-workgroup':self.wg1.pk
+            })
+        response = self.client.post(self.wizard_url, step_3_data)
+        self.assertEqual(response.status_code, 200)
+        wizard = response.context['wizard']
+        self.assertEqual(wizard['steps'].current, 'make_p')
+
+        # Now we make the property
+        step_4_data = {
+            self.wizard_form_name+'-current_step': 'make_p',
+            'make_p-name':"Animal type",
+        }
+
+        response = self.client.post(self.wizard_url, step_4_data)
+        wizard = response.context['wizard']
+        self.assertTrue('description' in response.context['wizard']['form'].errors.keys())
+        self.assertTrue('workgroup' in response.context['wizard']['form'].errors.keys())
+
+        # no "test item" yet.
+        self.assertFalse(models._concept.objects.filter(name="Test Item").exists())
+
+        # must submit a description at this step. But we are using a non-permitted workgroup.
+        step_4_data.update({
+            'make_p-description':"A wizard who can change shape.",
+            'make_p-workgroup':self.wg2.pk
+            })
+        response = self.client.post(self.wizard_url, step_4_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('workgroup' in response.context['wizard']['form'].errors.keys())
+
+        # must submit a description at this step. With the right workgroup
+        step_4_data.update({
+            'make_p-workgroup':self.wg1.pk
+            })
+        response = self.client.post(self.wizard_url, step_4_data)
+        self.assertEqual(response.status_code, 200)
+        wizard = response.context['wizard']
+        self.assertEqual(wizard['steps'].current, 'find_dec_results')
+
+        step_4_data.update(step_1_data)
+        step_4_data.update(step_2_data)
+        step_4_data.update(step_3_data)
+        self.assertEqual(response.status_code, 200)
+        wizard = response.context['wizard']
+        self.assertEqual(response.context['form'].initial['name'], 'Animagus--Animal type')
+
+
+        step_5_data = {}
+        step_5_data.update(step_1_data)
+        step_5_data.update(step_2_data)
+        step_5_data.update(step_3_data)
+        step_5_data.update(step_4_data)
+        step_5_data.update({self.wizard_form_name+'-current_step': 'find_dec_results',})
+
+        response = self.client.post(self.wizard_url, step_5_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('name' in response.context['wizard']['form'].errors.keys())
 
 
 """Ordinary. Wizarding. Level. Examinations. O.W.L.s. More commonly known as 'Owls'.
