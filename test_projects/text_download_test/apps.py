62714a935935583fb585b20c812eb261c6b596bc
theodore.therone@gmail.com <aristotle@ip-10-155-13-24.ec2.internal>
2015-03-01 04:57:41 +0000

    add extensions page test

---
+++ test_projects/text_download_test/apps.py
@@ -0,0 +1,5 @@
+from aristotle_mdr.apps import AristotleExtensionBaseConfig
+
+class DownloadTestMDRConfig(AristotleExtensionBaseConfig):
+    name = 'text_download_test'
+    verbose_name = "Aristotle Test Suite for testing downloads"
