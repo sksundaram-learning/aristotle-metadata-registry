d30149035c1be96a2a3cc2a2c6ddd78c50c44cbe
theodore.therone@gmail.com <aristotle@ip-10-138-111-216.ec2.internal>
2015-04-26 04:32:08 +0000

    more fies

--- aristotle_easy_installer.py
+++ aristotle_easy_installer.py
@@ -7,22 +7,26 @@ from subprocess import call
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 optional_modules = [
-    ("Aristotle Glossary Extension","#!aristotle-glossary!"),
-    ("Aristotle Dataset Extensions","#!aristotle-dse!")
-    ("Aristotle DDI Downloaders","#!aristotle-ddi!")
+    ("Aristotle Glossary Extension","#!aristotle_glossary!"),
+    ("Aristotle Dataset Extensions","#!aristotle_dse!"),
+    ("Aristotle DDI Downloaders","#!aristotle_ddi!")
 ]
 
 def install(package):
     pip.main(['install', package])
 
 def valid_input(prompt,match):
+    try:
+        input = raw_input
+    except:
+        pass
     for i in range(5):
         check = input(prompt)
-        if re.match(match, check_name):
+        if re.match(match, check):
             return check
     raise Exception
 
-def setup_mdr(name="",extensions=[]):
+def setup_mdr(name="",extensions=[],force_install=False,dry_install=False):
     name_regex = '^[a-z][a-z_]*$'
     if not re.match(name_regex, name):
         name = valid_input("Enter the system name for your registry (lowercase letters and underscores ONLY): ",name_regex)
@@ -42,23 +46,39 @@ def setup_mdr(name="",extensions=[]):
     yn = '^[YyNn]?$' # yes/no regex
     if not extensions:
         do_install = valid_input("Do you wish to install any additional Aristotle modules? (y/n): ", yn ).lower()
-        if do_install is 'y':
+        if do_install == 'y':
             print("Select extensions to install (y/n)")        
             for display, ext_token in optional_modules:
                 do_ext = valid_input("  %s: "%display, yn ).lower()
-                if do_ext is 'y':
+                if do_ext == 'y':
                     extensions.append(ext_token)
     if extensions:
         find_and_remove(name,extensions)
-    print("You can now locally test your installed registry by running the command './manage.py runserver'")
 
-def collect_static():
-    call(["django-admin", 'collectstatic'])
-    return call
+    if dry_install:
+        print("Performing dry run, no requirements installed.")
+        print("You can finish installing by running - pip install requirements.txt - from the %s directory"%name)
+        return 0
+    elif force_install:
+        print("Installing from requirements.txt")
+    else:
+        valid_input("Ready to install requirements? (y/n): ", yn)
+
+    if not dry_install:
+        print("Running django command to fetch all required static files")
+        collect_static(name)
+
+        print("You can now locally test your installed registry by running the command './manage.py runserver'")
+
+def collect_static(name):
+    call(["./%s/manage.py"%name, 'collectstatic'])
+    return call 
 
 def download_example_mdr():
-    command = "export https://github.com/aristotle-mdr/aristotle-metadata-registry/trunk/example_mdr/"
-    call(["svn", command])
+    print("Attempting to retrieve example registry")
+    command = "export"
+    arg = "https://github.com/legostormtroopr/aristotle-metadata-registry/branches/plato/example_mdr/"
+    call(["svn", command, arg])
     return call
 
 def find_and_remove(mydir,extensions):
@@ -83,7 +103,8 @@ def main(argv=None):
         argv = sys.argv
     try:
         try:
-            opts, args = getopt.getopt(argv[1:], "h", ["help"])
+            opts, args = getopt.getopt(argv[1:], "hn:df", ["help","name=","dry","force"])
+            opts = dict(opts)
         except getopt.error, msg:
              raise Usage(msg)
         # more code, unchanged
@@ -91,7 +112,15 @@ def main(argv=None):
         print >>sys.stderr, err.msg
         print >>sys.stderr, "for help use --help"
         return 2
-    return setup_mdr()
+    kwargs = {}
+    if '-n' in opts.keys() or '--name' in opts.keys():
+        kwargs['name']=opts.get('-n',opts.get('--name'))
+    if '-d' in args or '--dry' in args:
+        kwargs['dry_install']=True
+    if '-f' in args or '--force' in args:
+        kwargs['force_install']=True
+
+    return setup_mdr(**kwargs)
         
 if __name__ == "__main__":
     sys.exit(main())
