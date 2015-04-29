cf81bd59efb77208ec982d9d277a5235647387c7
theodore.therone@gmail.com <aristotle@ip-10-138-111-216.ec2.internal>
2015-04-29 22:08:45 +0000

    fixed location of example_mdr in installer

--- aristotle_easy_installer.py
+++ aristotle_easy_installer.py
@@ -123,7 +123,7 @@ def collect_static(name):
 def download_example_mdr():
     print("Attempting to retrieve example registry")
     command = "export"
-    arg = "https://github.com/legostormtroopr/aristotle-metadata-registry/branches/plato/example_mdr/"
+    arg = "https://github.com/aristotle-mdr/aristotle-metadata-registry/trunk/example_mdr/"
     call(["svn", command, arg])
     return call
 
