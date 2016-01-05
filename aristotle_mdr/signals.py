854e1d878f24fde6d029292572912adb463d2829
Thomas Sutton <me@thomas-sutton.id.au>
2016-01-05 13:21:00 +0800

    Style: resolve even more style warnings

--- aristotle_mdr/signals.py
+++ aristotle_mdr/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save, post_delete, pre_delete
 from reversion.signals import post_revision_commit
-import haystack.signals as signals # .RealtimeSignalProcessor as RealtimeSignalProcessor
+import haystack.signals as signals  # .RealtimeSignalProcessor as RealtimeSignalProcessor
 # Don't import aristotle_mdr.models directly, only pull in whats required,
 #  otherwise Haystack gets into a circular dependancy.
 
@@ -8,26 +8,28 @@ import haystack.signals as signals # .RealtimeSignalProcessor as RealtimeSignalP
 # Replace below with this when doing a dataload (shuts off Haystack)
 #    pass
 
+
 class AristotleSignalProcessor(signals.BaseSignalProcessor):
     def setup(self):
-        from aristotle_mdr.models import _concept,Workgroup
+        from aristotle_mdr.models import _concept, Workgroup
         # post_save.connect(self.handle_concept_save, sender=_concept)
         post_revision_commit.connect(self.handle_concept_revision)
         pre_delete.connect(self.handle_concept_delete, sender=_concept)
-        super(AristotleSignalProcessor,self).setup()
+        super(AristotleSignalProcessor, self).setup()
 
-    def teardown(self): # pragma: no cover
+    def teardown(self):  # pragma: no cover
         from aristotle_mdr.models import _concept
         # post_save.disconnect(self.handle_concept_save, sender=_concept)
         post_revision_commit.disconnect(self.handle_concept_revision)
         pre_delete.disconnect(self.handle_concept_delete, sender=_concept)
-        super(AristotleSignalProcessor,self).teardown()
+        super(AristotleSignalProcessor, self).teardown()
 
     def handle_concept_revision(self, instances, **kwargs):
         from aristotle_mdr.models import _concept
         for instance in instances:
             if isinstance(instance, _concept) and type(instance) is not _concept:
-                self.handle_save(instance.__class__,instance)
+                self.handle_save(instance.__class__, instance)
+
     """
     # Keeping this just in case, but its unlikely to be used again after the
     transition to post_revision_commit signals.
@@ -39,4 +41,4 @@ class AristotleSignalProcessor(signals.BaseSignalProcessor):
     def handle_concept_delete(self, sender, instance, **kwargs):
         # Delete index *before* the object, as we need to query it to check the actual subclass.
         obj = instance.item
-        self.handle_delete(obj.__class__,obj, **kwargs)
+        self.handle_delete(obj.__class__, obj, **kwargs)
--- aristotle_mdr/urls.py
+++ aristotle_mdr/urls.py
@@ -12,32 +12,35 @@ from aristotle_mdr.views.user_pages import friendly_redirect_login
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     # Examples:
-    url(r'^login/?$', friendly_redirect_login,name='friendly_login'),
-    url(r'^logout/?$', 'django.contrib.auth.views.logout',
-        {'next_page': '/'}),
+    url(r'^login/?$', friendly_redirect_login, name='friendly_login'),
+    url(r'^logout/?$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^autocomplete/', include('autocomplete_light.urls')),
-    url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
+    url(r'^grappelli/', include('grappelli.urls')),  # grappelli URLS
     url(r'^ckeditor/', include('ckeditor.urls')),
     url(r'^account/notifications/', include(notifications.urls)),
     url(r'^account/password/reset/$', password_reset),  # , {'template_name': 'my_templates/password_reset.html'}
     url(r'^account/password/reset_done/$', password_reset),  # , {'template_name': 'my_templates/password_reset.html'}
-    url(r'^', include('aristotle_mdr.urls_aristotle',app_name="aristotle_mdr",namespace="aristotle")),
+    url(r'^', include('aristotle_mdr.urls_aristotle', app_name="aristotle_mdr", namespace="aristotle")),
 
-    url(r'^user/password/reset/$',
+    url(
+        r'^user/password/reset/$',
         'django.contrib.auth.views.password_reset',
-        {'post_reset_redirect' : '/user/password/reset/done/'},
-        name="password_reset"),
-    url(r'^user/password/reset/done/$',
+        {'post_reset_redirect': '/user/password/reset/done/'},
+        name="password_reset"
+    ),
+    url(
+        r'^user/password/reset/done/$',
         'django.contrib.auth.views.password_reset_done',
         name="password_reset_done"
-        ),
+    ),
     (r'^user/password/reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
         'django.contrib.auth.views.password_reset_confirm',
-        {'post_reset_redirect' : '/user/password/done/'}),
+        {'post_reset_redirect': '/user/password/done/'}),
     (r'^user/password/done/$',
         'django.contrib.auth.views.password_reset_complete'),
 
