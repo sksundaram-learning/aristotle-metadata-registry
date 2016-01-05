65069b14656e43d0614bdb8c2169d5703d21a8bb
Thomas Sutton <me@thomas-sutton.id.au>
2016-01-05 12:42:37 +0800

    Style: more style cleanup

--- aristotle_mdr/management/__init__.py
+++ aristotle_mdr/management/__init__.py
@@ -4,8 +4,10 @@ import aristotle_mdr
 import sys
 from django.conf import settings
 
-def loadDefaultData(**kwargs): # pragma: no cover
+
+def loadDefaultData(**kwargs):  # pragma: no cover
     aristotle_mdr.models.defaultData()
+
 
 def configureSystemUser(**kwargs):
     # Make a system "user" this is used for when the systems need to post messages
@@ -16,16 +18,19 @@ def configureSystemUser(**kwargs):
         system.is_staff = False
         system.save()
 
+
 signals.post_syncdb.connect(configureSystemUser, sender=aristotle_mdr.models)
 signals.post_syncdb.connect(loadDefaultData, sender=aristotle_mdr.models)
 
 
 # These are not used during testing.
-def loadExampleData(**kwargs): # pragma: no cover
+def loadExampleData(**kwargs):  # pragma: no cover
     print("Loading Aristotle-MDR test data because DEBUG is set to True.")
     # Disable notification signals disabled as not everything will be setup yet.
     signals.post_save.disconnect(aristotle_mdr.models.concept_saved)
     aristotle_mdr.models.exampleData()
     signals.post_save.connect(aristotle_mdr.models.concept_saved)
+
+
 if 'test' not in sys.argv and getattr(settings, 'DEBUG', "") == True:  # pragma: no cover
     signals.post_syncdb.connect(loadExampleData, sender=aristotle_mdr.models)
--- aristotle_mdr/management/commands/recache_workgroup_item_visibility.py
+++ aristotle_mdr/management/commands/recache_workgroup_item_visibility.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from aristotle_mdr.models import Workgroup
 
+
 class Command(BaseCommand):
     args = '<workgroup_id workgroup_id ...>'
     help = 'Recomputes and caches the public andlocked statuses for the given workgroup(s). This is useful if the registration authorities associated with a workgroup change.'
@@ -10,12 +11,12 @@ class Command(BaseCommand):
         for wg_id in options['wg']:
             try:
                 wg = Workgroup.objects.get(pk=int(wg_id))
-            except Workgroup.DoesNotExist: # pragma: no cover
+            except Workgroup.DoesNotExist:  # pragma: no cover
                 raise CommandError('Workgroup "%s" does not exist' % wg_id)
-            self.stdout.write('Beginning update for items in Workgroup "%s" (id:%s)' % (wg.name,wg_id,))
+            self.stdout.write('Beginning update for items in Workgroup "%s" (id:%s)' % (wg.name, wg_id))
 
             for item in wg.items.all():
                 item.recache_states()
                 connections['default'].get_unified_index().get_index(item.item.__class__).update_object(item.item)
 
-            self.stdout.write('Successfully updated items in Workgroup "%s" (id:%s)' % (wg.name,wg_id,))
+            self.stdout.write('Successfully updated items in Workgroup "%s" (id:%s)' % (wg.name, wg_id))
