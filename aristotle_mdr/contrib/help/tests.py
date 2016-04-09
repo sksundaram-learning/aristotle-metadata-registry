672414003a3111f0d08bef850eacbfb6b4b7a245
Samuel Spencer <theodore.therone@gmail.com>
2016-04-09 04:56:50 +0000

    peppy :hurtrealbad:

--- aristotle_mdr/contrib/help/tests.py
+++ aristotle_mdr/contrib/help/tests.py
@@ -3,9 +3,11 @@ from django.core.urlresolvers import reverse
 from django.test import TestCase, override_settings, modify_settings
 from django.test.utils import setup_test_environment
 
-setup_test_environment()
 from django.core.management import call_command
 from aristotle_mdr.contrib.help import models
+
+setup_test_environment()
+
 
 class TestHelpPagesLoad(TestCase):
     def test_help_pages_load(self):
@@ -13,7 +15,6 @@ class TestHelpPagesLoad(TestCase):
         call_command('loadhelp', 'aristotle_help/*')
         count_2 = models.HelpPage.objects.all().count()
         self.assertTrue(count_2 > count_1)
-        
 
     def test_concept_help_pages_load(self):
         count_1 = models.ConceptHelp.objects.all().count()
