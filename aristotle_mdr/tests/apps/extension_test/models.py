10341cf7e4766f0d2995322be5317d66249c7371
Samuel Spencer <theodore.therone@gmail.com>
2016-07-15 03:50:16 +0000

    catchup

--- aristotle_mdr/tests/apps/extension_test/models.py
+++ aristotle_mdr/tests/apps/extension_test/models.py
@@ -2,16 +2,16 @@ from __future__ import unicode_literals
 
 # Start of the question model
 
-from aristotle_mdr import models as MDR
+import aristotle_mdr
 from django.db import models
 
 
-class Question(MDR.concept):
+class Question(aristotle_mdr.models.concept):
     template = "extension_test/concepts/question.html"
     questionText = models.TextField(blank=True, null=True)
     responseLength = models.PositiveIntegerField(blank=True, null=True)
     collectedDataElement = models.ForeignKey(
-        MDR.DataElement,
+        aristotle_mdr.models.DataElement,
         related_name="questions",
         null=True,
         blank=True
@@ -20,7 +20,7 @@ class Question(MDR.concept):
 # End of the question model
 
 
-class Questionnaire(MDR.concept):
+class Questionnaire(aristotle_mdr.models.concept):
     # Questionnaire is a test of a lazy developer who has done the bare minimum
     # To get an object in the system. This is a test of how little a dev can to
     # get a functional object. Ideally the string 'Questionnaire' should exist only here.
@@ -34,7 +34,7 @@ class Questionnaire(MDR.concept):
         blank=True
     )
     respondent_classes = models.ManyToManyField(
-        MDR.ObjectClass,
+        aristotle_mdr.models.ObjectClass,
         through='TargetRespondentClass'
     )
 
@@ -46,19 +46,19 @@ class Questionnaire(MDR.concept):
                 self.questions.all().order_by('name')
             ),
             (
-                MDR.DataElement,
-                MDR.DataElement.objects.filter(questions__questionnaires=self).order_by('name')
+                aristotle_mdr.models.DataElement,
+                aristotle_mdr.models.DataElement.objects.filter(questions__questionnaires=self).order_by('name')
             ),
         ]
     # End of get_download_items
 
 
 # This is a pretty contrived testing model
-class TargetRespondentClass(MDR.aristotleComponent):
+class TargetRespondentClass(aristotle_mdr.models.aristotleComponent):
     @property
     def parentItem(self):
         return self.questionnaire
 
     questionnaire = models.ForeignKey('Questionnaire')
-    respondent_class = models.ForeignKey(MDR.ObjectClass)
+    respondent_class = models.ForeignKey(aristotle_mdr.models.ObjectClass)
     rationale = models.TextField(blank=True, null=True)
