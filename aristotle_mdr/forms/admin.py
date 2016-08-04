8acecba9a67cbd7fc892bbc38e2fbca2a9fb22f3
Samuel Spencer <theodore.therone@gmail.com>
2016-08-04 03:34:47 +0000

    fix admin

--- aristotle_mdr/forms/admin.py
+++ aristotle_mdr/forms/admin.py
@@ -4,6 +4,7 @@ from django.utils.translation import ugettext_lazy as _
 
 import aristotle_mdr.models as MDR
 import aristotle_mdr.widgets as widgets
+from aristotle_mdr.contrib.autocomplete.widgets import ConceptAutocompleteSelectMultiple
 from aristotle_mdr.perms import user_can_edit
 from aristotle_mdr.utils import concept_to_clone_dict
 from aristotle_mdr.forms.creation_wizards import WorkgroupVerificationMixin, ConceptForm
@@ -81,16 +82,13 @@ class AdminConceptForm(ConceptForm, WorkgroupVerificationMixin):
         super(AdminConceptForm, self).__init__(*args, **kwargs)
         if self.instance and not clone:
             self.itemtype = self.instance.__class__
-            # self.fields['deprecated'] = forms.ModelMultipleChoiceField(
-            #     required=False,
-            #     label="Supersedes",
-            #     queryset=self.itemtype.objects.all(),
-            # )
-            # if self.instance.get_autocomplete_name() in autocomplete_light.registry.keys():
-            #     # if there is an autocomplete for this item, then replace it
-            #     # TODO: when autocomplete respects queryset these can be done automatically
-            #     self.fields['deprecated'].widget = autocomplete_light.MultipleChoiceWidget(self.instance.get_autocomplete_name())
-            # self.fields['deprecated'].initial = self.instance.supersedes.all()
+            self.fields['deprecated'] = forms.ModelMultipleChoiceField(
+                required=False,
+                label="Supersedes",
+                queryset=self.itemtype.objects.all(),
+                widget=ConceptAutocompleteSelectMultiple(model=self._meta.model)
+            )
+            self.fields['deprecated'].initial = self.instance.supersedes.all()
 
         if name_suggest_fields:
             self.fields['name'].widget = widgets.NameSuggestInput(name_suggest_fields=name_suggest_fields, separator=separator)
