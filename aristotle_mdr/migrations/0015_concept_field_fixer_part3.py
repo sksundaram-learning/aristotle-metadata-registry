daf35639227e7acfde896d0da20cc986e41b4580
Samuel Spencer <theodore.therone@gmail.com>
2016-08-29 02:40:38 +0000

    remove unneeded migrations

--- aristotle_mdr/migrations/0015_concept_field_fixer_part3.py
+++ aristotle_mdr/migrations/0015_concept_field_fixer_part3.py
@@ -6,8 +6,4 @@ from aristotle_mdr.utils.migrations import ConceptMigrationRenameConceptFields
 class Migration(ConceptMigrationRenameConceptFields):
     dependencies = [
         ('aristotle_mdr', '0014_concept_field_fixer_part2'),
-        ('aristotle_dse', '0012_fix_concept_fields'),
-        ('aristotle_glossary', '0003_fix_concept_fields'),
-        ('comet', '0008_fix_concept_fields'),
-        ('mallard_qr', '0005_fix_concept_fields'),
     ]
