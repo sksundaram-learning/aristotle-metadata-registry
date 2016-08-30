dd9fc287a38cfa4293819cc0a7db975378cd0bd6
Samuel Spencer <theodore.therone@gmail.com>
2016-08-30 01:47:00 +0000

    pep :rage2:

--- aristotle_mdr/utils/migrations.py
+++ aristotle_mdr/utils/migrations.py
@@ -28,7 +28,7 @@ class MoveConceptFields(Operation):
 
     def database_forwards(self, app_label, schema_editor, from_state, to_state):
 
-        if True: #schema_editor.connection.vendor == 'sqlite':
+        if True:  # schema_editor.connection.vendor == 'sqlite':
             concept_table_name = "%s_%s" % (app_label, self.model_name)
             for column in [
                 'comments', 'origin_URI', 'references', 'responsible_organisation',
