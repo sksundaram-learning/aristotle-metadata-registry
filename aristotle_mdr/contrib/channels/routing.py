884c8631b1467c72cd5afac709672db782094562
Samuel Spencer <theodore.therone@gmail.com>
2016-09-12 23:34:03 +0000

    peppy

--- aristotle_mdr/contrib/channels/routing.py
+++ aristotle_mdr/contrib/channels/routing.py
@@ -1,8 +1,10 @@
 from channels.routing import route, route_class, include
 from haystack_channels.routing import channel_routing as haystack_routing
 
+
 def module_route(mod_route):
-    return route(mod_route,mod_route)
+    return route(mod_route, mod_route)
+
 
 channel_routing = [
     module_route("aristotle_mdr.contrib.channels.concept_changes.concept_saved"),
