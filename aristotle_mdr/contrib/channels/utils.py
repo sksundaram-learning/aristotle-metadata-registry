77c61d8c557e6b91a3460d8917b55257988b09a2
Samuel Spencer <theodore.therone@gmail.com>
2016-09-14 07:26:35 +0000

    pep8

--- aristotle_mdr/contrib/channels/utils.py
+++ aristotle_mdr/contrib/channels/utils.py
@@ -6,14 +6,14 @@ def fire(channel, obj=None, **kwargs):
     from django.utils.module_loading import import_string
     message = kwargs
     if hasattr(settings, 'CHANNEL_LAYERS'):
-        message.update({'__object___':
-            {
-                'pk':instance.pk,
-                'app_label':instance._meta.app_label,
-                'model_name':instance._meta.model_name,
+        message.update({
+            '__object___': {
+                'pk': instance.pk,
+                'app_label': instance._meta.app_label,
+                'model_name': instance._meta.model_name,
             }
         })
         c = Channel("aristotle_mdr.contrib.channels.%s" % channel).send(message)
     else:
-        message.update({'__object__':{'object':obj}})
+        message.update({'__object__': {'object': obj}})
         import_string("aristotle_mdr.contrib.channels.%s" % channel)(message)
