40d1aaa6a747a3e9c43720d3a131247d95632802
Samuel Spencer <theodore.therone@gmail.com>
2016-10-09 23:17:52 +0000

    fix migration

--- aristotle_mdr/migrations/0017_add_organisations.py
+++ aristotle_mdr/migrations/0017_add_organisations.py
@@ -52,7 +52,6 @@ class CopyFieldOnSingleTable(Operation):
         base_query = "UPDATE %s_%s SET \"%s\" = \"%s\"" % tuple(
             [app_label, self.model_name, self.field_to_name, self.field_from_name]
         )
-        print base_query
         schema_editor.execute(base_query)
 
     def describe(self):
@@ -66,7 +65,7 @@ class Migration(migrations.Migration):
         ('aristotle_mdr', '0016_auto_20160919_1939'),
     ]
 
-    operations = [
+    state_operations = [
         migrations.CreateModel(
             name='Organization',
             fields=[
@@ -81,11 +80,6 @@ class Migration(migrations.Migration):
             options={
                 'abstract': False,
             },
-        ),
-        CopyFieldsBetweenTables(
-            model_from_name='registrationauthority',
-            model_to_name='organization',
-            columns=['created', 'definition', 'id', 'modified', 'name' ],
         ),
         migrations.RemoveField(
             model_name='registrationauthority',
@@ -107,38 +101,11 @@ class Migration(migrations.Migration):
             model_name='registrationauthority',
             name='name',
         ),
-        migrations.AddField(
-            model_name='registrationauthority',
-            name='temp_organization_ptr',
-            field=models.IntegerField(null=True),
-        ),
 
-        # Copy the id field, then remove it as postgres complains about having two primary keys
-        CopyFieldOnSingleTable(
+        migrations.RenameField(
             model_name='registrationauthority',
-            field_from_name='id',
-            field_to_name='temp_organization_ptr',
-        ),
-        migrations.RemoveField(
-            model_name='registrationauthority',
-            name='id',
-        ),
-        migrations.AddField(
-            model_name='registrationauthority',
-            name='organization_ptr',
-            field=models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, null=True, serialize=False, to='aristotle_mdr.Organization'),
-            preserve_default=False,
-        ),
-
-        CopyFieldOnSingleTable(
-            model_name='registrationauthority',
-            field_from_name='temp_organization_ptr',
-            field_to_name='organization_ptr_id',
-        ),
-
-        migrations.RemoveField(
-            model_name='registrationauthority',
-            name='temp_organization_ptr',
+            old_name='id',
+            new_name='organization_ptr'
         ),
 
         migrations.AlterField(
@@ -148,4 +115,112 @@ class Migration(migrations.Migration):
             preserve_default=False,
         ),
 
+    ]
+
+    database_operations = [
+        migrations.CreateModel(
+            name='Organization',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, verbose_name='created', editable=False)),
+                ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, verbose_name='modified', editable=False)),
+                ('name', models.TextField(help_text='The primary name used for human identification purposes.')),
+                ('definition', ckeditor_uploader.fields.RichTextUploadingField(help_text='Representation of a concept by a descriptive statement which serves to differentiate it from related concepts. (3.2.39)', verbose_name='definition')),
+                ('uri', models.URLField(help_text='uri for Organization', null=True, blank=True)),
+                ('managers', models.ManyToManyField(related_name='organization_manager_in', verbose_name='Managers', to=settings.AUTH_USER_MODEL, blank=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.RemoveField(
+            model_name='registrationauthority',
+            name='created',
+        ),
+        migrations.RemoveField(
+            model_name='registrationauthority',
+            name='definition',
+        ),
+        migrations.RemoveField(
+            model_name='registrationauthority',
+            name='managers',
+        ),
+        migrations.RemoveField(
+            model_name='registrationauthority',
+            name='modified',
+        ),
+        migrations.RemoveField(
+            model_name='registrationauthority',
+            name='name',
+        ),
+
+        migrations.RenameField(
+            model_name='registrationauthority',
+            old_name='id',
+            new_name='organization_ptr'
+        ),
+
+        migrations.AlterField(
+            model_name='registrationauthority',
+            name='organization_ptr',
+            field=models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='aristotle_mdr.Organization'),
+            preserve_default=False,
+        ),
+
+        # This is a fake model to capture the M2M
+        migrations.CreateModel(
+            name='registrationauthority_registrars_temp',
+            fields=[
+                ('registrationauthority_id', models.IntegerField()),
+                ('user_id', models.IntegerField()),
+            ]
+        ),
+        
+        # Fix registrars
+        CopyFieldsBetweenTables(
+            model_from_name='registrationauthority_registrars',
+            model_to_name='registrationauthority_registrars_temp',
+            columns=['registrationauthority_id', 'user_id'],
+        ),
+
+        migrations.RemoveField(
+            model_name='registrationauthority',
+            name='registrars',
+        ),
+        
+        migrations.AddField(
+            model_name='registrationauthority',
+            name='registrars',
+            field=models.ManyToManyField(related_name='registrar_in', to=settings.AUTH_USER_MODEL, blank=True),
+            preserve_default=True
+        ),
+        CopyFieldsBetweenTables(
+            model_from_name='registrationauthority_registrars_temp',
+            model_to_name='registrationauthority_registrars',
+            columns=['registrationauthority_id', 'user_id'],
+        ),
+
+        migrations.DeleteModel('registrationauthority_registrars_temp'),
+
+        # Fix status and review request fields
+        migrations.AlterField(
+            model_name='status',
+            name='registrationAuthority',
+            field=models.ForeignKey(to='aristotle_mdr.RegistrationAuthority'),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='reviewrequest',
+            name='registrationAuthority',
+            field=models.ForeignKey(to='aristotle_mdr.RegistrationAuthority'),
+            preserve_default=True,
+        ),
+
+    ]
+
+
+    operations = [
+        migrations.SeparateDatabaseAndState(
+            database_operations=database_operations,
+            state_operations=state_operations)
     ]
